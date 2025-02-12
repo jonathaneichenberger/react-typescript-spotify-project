e77a90a087754b320f9c707afd4c1f7f34b28e61
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-12 19:26:23 -0300

    Começado a compor o component TopList

--- src/components/main-component/top-list-component/TopList.tsx
+++ src/components/main-component/top-list-component/TopList.tsx
@@ -3,6 +3,15 @@ import '../Main.css'
 const TopList = () => {
   return (
     <div className="item-list">
+      <div className="item-list__header">
+        <h2>Artistas populares</h2>
+        <a className="item-list__link" href="/">
+          Mostrar tudo
+        </a>
+      </div>
+      <div className="item-list__container">
+
+      </div>
 
     </div>
   );
