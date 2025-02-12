10e21dfd5ad1f6832035fae2244f0df9601a0aa7
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-12 17:59:15 -0300

    Ajustando o component Header

---
+++ src/components/header-component/Header.tsx
@@ -0,0 +1,16 @@
+import logoSpotify from "../../assets/logo/spotify-logo.png";
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={logoSpotify} alt="Logo do Spotify" />
+
+      <a className="header__link" href="/">
+        <h1>Spotify</h1>
+      </a>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import Header from "../components/Header";
-import Main from "../components/Main";
+import Header from "../components/header-component/Header";
+import Main from "../components/main-component/Main";
 
 const home = () => {
   return (
