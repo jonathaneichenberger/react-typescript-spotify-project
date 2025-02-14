5fdaeb8b609eddc31c9f9202464b939e948ab517
Jonathan Eichenberger <djohnni@hotmail.com>
2025-02-14 10:10:34 -0300

    Recomeçando projeto e  colocando o css no index.css por enquanto

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,8 @@
-import Header from "../components/header-component/Header";
-import Main from "../components/main-component/Main";
 
 const home = () => {
   return (
     <>
-      <Header />
-      <Main />
+      
     </>
   );
 };
